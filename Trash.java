baad8ffef9f4b988c4623aeab65f73b947f694fd
Daydream <112185088+DaydreamNitemare@users.noreply.github.com>
2022-11-04 16:07:26 -0500

    Trash Class
    This class manages all of the Tiles that are placed into the trash

---
+++ Trash.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+public class Trash
+{
+    ArrayList<Tile> trashArray;
+
+    public Trash()
+    {
+        trashArray = new ArrayList<>();
+    }
+
+    public ArrayList<Tile> getTrash()
+    {
+        return trashArray;
+    }
+
+    public void addToTrash(Tile t)
+    {
+        trashArray.add(t);
+    }
+
+    public Tile removeFromTrash(int pos)
+    {
+        return trashArray.remove(pos);
+    }
+}
+
+//this is a test
