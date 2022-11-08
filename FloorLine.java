6cd7a2d507ea23a8d02e1ac90b7557535e63ff12
Daydream <112185088+DaydreamNitemare@users.noreply.github.com>
2022-11-08 12:20:57 -0600

    Update FloorLine.java
    Changed how tiles are added to a players FloorLine.

--- FloorLine.java
+++ FloorLine.java
@@ -1,15 +1,25 @@
 import java.util.*;
 public class FloorLine {
     private ArrayList<Tile> floorLine;
-    public FloorLine(){
+    public FloorLine()
+    {
         floorLine = new ArrayList<Tile>();
     }
-    public void add(Tile t){
-        floorLine.add(t);
+    public void add(Tile t, Trash trash)
+    {
+        if(floorLine.size() < 7)
+            floorLine.add(t);
+        else
+            trash.addToTrash(t);
     }
-    public int score(){
+
+    /*
+    This needs fixing
+     */
+    public int score()
+    {
         int s =0;
-        for(int i =0; i<floorLine.size(); i++){
+        for(int i = 0; i<floorLine.size(); i++){
             if(i<2){
                 s--;
             }
