dda1b658ee08f401feddb84bf754bbc7f7b89e7b
AshmeetC <68480568+AshmeetC@users.noreply.github.com>
2022-11-09 12:12:42 -0600

    Click class outline - to be worked on tonight

---
+++ Click.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.awt.*;
+import javax.swing.*;
+public class Click {
+    private int xVal, yVal;
+    private Player current;
+    public Click(int x, int y, Player p) {
+        xVal=x;
+        yVal=y;
+        current=p;
+    }
+
+}
