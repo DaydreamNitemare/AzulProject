d5c6db5866aed6c3a8ed7c6386b33d13535a4246
Daydream <112185088+DaydreamNitemare@users.noreply.github.com>
2022-11-10 17:37:57 -0600

    Update GameFrame.java
    Fixed some of the graphics on the GameFrame and made sure everything appears as it should. Also started the development of the MouseListener.

--- GameFrame.java
+++ GameFrame.java
@@ -11,56 +11,67 @@ public class GameFrame extends JFrame implements ActionListener, MouseListener
 {
 
     Game game;
-    BufferedImage gameback;
-    public GameFrame()
+    BufferedImage gameBack;
+    JButton nextButton, submitButton;
+    JRadioButton selectionMenu;
+    public GameFrame() //constructor
     {
-     //   game = new Game();
+        this.setSize(1440, 1024);
+        this.setResizable(false);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(null);
 
-        try
-        {
-            //where all of the BufferedImage variables will be instantiated
-         gameback= ImageIO.read(MainFrame.class.getResource("/images/gamebackground.png"));
-            }
-        catch (Exception E) {
-            System.out.println("Error with instantiating the images in MainFrame");
+        try {
+            //where all the BufferedImage variables will be instantiated
+            gameBack = ImageIO.read(MainFrame.class.getResource("/images/gameBackground.png"));
+        } catch (Exception E) {
+            System.out.println("Error with instantiating the images in GameFrame");
             return;
         }
 
-
-
-
-
-
-
-
-
+        this.addMouseListener(this);
+        this.setVisible(true);
     }
+
+    /***************** PAINT *********************/
+
     public void paint(Graphics g)
     {
         //whatever goes in here, shall go in here
-        g.drawImage(gameback,0,0,getWidth(),getHeight(),null);
+        g.drawImage(gameBack,0,30,getWidth(),getHeight() - 30,null);
     }
-
-
 
     /************************* ACTION PERFORMED ********************/
 
-    public void actionPerformed(ActionEvent e)
+    public void actionPerformed(ActionEvent e) //really only necessary for the next player and submit buttons
     {
-
+        //dont forget to add ActionListeners for the JButtons and JRadioButtons
     }
 
 
     /********************* MOUSE CLICKED ************************/
 
-    public void mouseClicked(MouseEvent e)
+    public void mouseClicked(MouseEvent e) //will be short because most code will be in the Click class
     {
-        if(game.getCurrentPlayer().canDraw())
-        {
-            Click c = new Click(e.getX(), e.getY(), game.getCurrentPlayer());
-            c.draw();
-        }
+        int x = e.getX();
+        int y = e.getY();
 
+        System.out.println("loc of click is (" + x + " , " + y + ")");
+
+        /*
+        if(game.getCurrentPlayer().playerCanDraw())
+        {
+            Click c = new Click(x, y, game.getCurrentPlayer());
+            while(!c.draw())
+                return;
+            game.getCurrentPlayer().changeCanDraw(false);
+            game.getCurrentPlayer().canPlay(true);
+
+            //below here, write the code that will display the JRadioButtons to have the player choose the rows.
+
+            //then, include a check to see if there are no more tiles to then end the round/game
+          }
+        */
     }
 
     public void mousePressed(MouseEvent e){}
