d740702077785e65c59f575ac0c92370c9b2dc9e
lucca-t <102130161+lucca-t@users.noreply.github.com>
2022-11-10 12:17:52 -0600

    Update GameFrame.java
    added background image

--- GameFrame.java
+++ GameFrame.java
@@ -1,19 +1,45 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
 
 public class GameFrame extends JFrame implements ActionListener, MouseListener
 {
 
     Game game;
-
+    BufferedImage gameback;
     public GameFrame()
     {
-        game = new Game();
-    }
+     //   game = new Game();
 
+        try
+        {
+            //where all of the BufferedImage variables will be instantiated
+         gameback= ImageIO.read(MainFrame.class.getResource("/images/gamebackground.png"));
+            }
+        catch (Exception E) {
+            System.out.println("Error with instantiating the images in MainFrame");
+            return;
+        }
+
+
+
+
+
+
+
+
+
+    }
+    public void paint(Graphics g)
+    {
+        //whatever goes in here, shall go in here
+        g.drawImage(gameback,0,0,getWidth(),getHeight(),null);
+    }
 
 
 
@@ -31,7 +57,7 @@ public class GameFrame extends JFrame implements ActionListener, MouseListener
     {
         if(game.getCurrentPlayer().canDraw())
         {
-            Click c = new Click(e.getX(), e.getY(), game.getCurrentPlayer(), game.getFactories());
+            Click c = new Click(e.getX(), e.getY(), game.getCurrentPlayer());
             c.draw();
         }
 
