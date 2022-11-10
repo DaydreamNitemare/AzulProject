9f5844692da4eccb5bb53ef83c0d7f82301eee03
lucca-t <102130161+lucca-t@users.noreply.github.com>
2022-11-10 11:57:56 -0600

    Update MainFrame.java
    added the clicks and stuff and background

--- MainFrame.java
+++ MainFrame.java
@@ -1,6 +1,8 @@
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
 import javax.swing.*;
@@ -11,11 +13,13 @@ import javax.swing.*;
 //black 3
 //white 4
 
-public class MainFrame extends JFrame implements ActionListener {
+public class MainFrame extends JFrame implements ActionListener, MouseListener {
     final int WIDTH=1440; //width
     final int HEIGHT=1024; //height
 
     //add the BufferedImages here. Make sure to add them onto the GitHub pls
+    BufferedImage title,startback;
+    ImageIcon icon;
     JButton start; //start button. Feel free to add more buttons here but one at a time to not get confused.
     public MainFrame() //constructor
     {
@@ -27,6 +31,11 @@ public class MainFrame extends JFrame implements ActionListener {
         try
         {
             //where all of the BufferedImage variables will be instantiated
+
+            startback= ImageIO.read(MainFrame.class.getResource("/images/startbackground.png"));
+            //icon=newImageIcon
+
+
         }
         catch (Exception E) {
             System.out.println("Error with instantiating the images in MainFrame");
@@ -48,7 +57,7 @@ public class MainFrame extends JFrame implements ActionListener {
 
         this.add(start); //adds the start button to the frame
         this.setVisible(true);
-
+        addMouseListener(this);
 
     }
 
@@ -69,6 +78,32 @@ public class MainFrame extends JFrame implements ActionListener {
     public void paint(Graphics g)
     {
         //whatever goes in here, shall go in here
+        g.drawImage(startback,0,0,getWidth(),getHeight(),null);
     }
 
+
+    public void mousePressed(MouseEvent e) {}
+    public void mouseReleased(MouseEvent e) {}
+    public void mouseEntered(MouseEvent e) {}
+    public void mouseExited(MouseEvent e) {}
+    public void mouseClicked(MouseEvent e) {
+        int x = e.getX();
+        int y = e.getY();
+        System.out.println("oc is (" + x + "," + y + ")");
+
+        //start button mouselistener
+        if(x>550&&x<890&&y>690&&y<790){
+            System.out.println("Start Game");
+            this.dispose();
+            //new GameFrame();
+        }
+        //How to Play button mouselistener
+        if(x>460&&x<700&&y>810&&y<870){
+            System.out.println("How To Play");
+        }
+        //Credits button mouselistener
+        if(x>740&&x<970&&y>810&&y<870){
+            System.out.println("Credits");
+        }
+    }
 }
