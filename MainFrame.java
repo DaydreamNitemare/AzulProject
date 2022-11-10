e1a3f2b12e810ec7e89a051d356d8b6351a66719
Daydream <112185088+DaydreamNitemare@users.noreply.github.com>
2022-11-09 19:56:37 -0600

    Update MainFrame.java
    This is my updated version of the MainFrame. As one can see, I removed a lot of aspects of the first MainFrame draft as it was buggy and cluttered. Although it does not do much now, the comments provide a sufficient explanation of what is occurring. The reason I made this edit into a new branch is so that Lucca can have his version and mine side by side in order to add what ever I removed as efficiently as possible. Thanks Lucca!

--- MainFrame.java
+++ MainFrame.java
@@ -5,88 +5,70 @@ import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 
-import static java.lang.System.out;
-
 //blue 0
-//white 1
-//black 2
-//red 3
-//yellow 4
+//yellow 1
+//red 2
+//black 3
+//white 4
+
 public class MainFrame extends JFrame implements ActionListener {
-    final static int WIDTH=1440;
-    final static int HEIGHT=1024;
-    private BufferedImage title,temp1,startback;
-    private JButton start,howTo,cred;
-    private ImageIcon icon;
-    public MainFrame(String str) {
-        //super(str);
-        setSize (WIDTH, HEIGHT);
-        setResizable(false);
-        setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
-        setVisible(true);
+    final int WIDTH=1440; //width
+    final int HEIGHT=1024; //height
 
-        try {
-            title=ImageIO.read(MainFrame.class.getResource("/images/title.png"));
-            startback= ImageIO.read(MainFrame.class.getResource("/images/startbackground.png"));
-            icon =new ImageIcon(MainFrame.class.getResource("/images/invisible.png"));
+    //add the BufferedImages here. Make sure to add them onto the GitHub pls
+    JButton start; //start button. Feel free to add more buttons here but one at a time to not get confused.
+    public MainFrame() //constructor
+    {
+        this.setSize (WIDTH, HEIGHT);
+        this.setResizable(false);
+        this.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
+        this.setLayout(null);
 
+        try
+        {
+            //where all of the BufferedImage variables will be instantiated
         }
         catch (Exception E) {
-            out.println("Exception error");
+            System.out.println("Error with instantiating the images in MainFrame");
             return;
-        } start = new JButton();
-        howTo=new JButton();
-        add(start);
-
-
-        add(howTo);
-        drawStart();
-
-        repaint();
-
-    }
-
-
-    public void actionPerformed(ActionEvent e) {
-        if(e.getSource()==start) {
-            //out.println("start");
-            start.setVisible(true);
-            out.println("Start Game");
         }
-        repaint();
+
+        /************************ START BUTTON ************************/
+
+        start = new JButton(); //instantiates the button
+        //these next few lines make the button active but invisible. feel free to add an image or set opaque true to debug
+        start.setOpaque(false);
         start.setFocusable(false);
-        start.addActionListener(this);
-        start.setVisible(true);
-        start.setEnabled(true);
-        start.setBounds(540,353,200,100);
-        //start.setBorder(new RoundedBorder(30));
-        //start.setBorderPainted(false);
-       // start.setContentAreaFilled(false);
-        //start.setOpaque(false);
-        //start.setOpaque(false);
-       // start.setContentAreaFilled(false);
-       // start.setBorderPainted(false);
-        howTo.setText("How To");
-        howTo.setFocusable(false);
-        howTo.addActionListener(this);
-        howTo.setVisible(false);
-        howTo.setBounds(100,100,100,100);
-        howTo.setSize(100   ,100);
+        start.setContentAreaFilled(false);
+        start.setBorderPainted(false);
+        //it's these 4 above
+        start.addActionListener(this); //adds an action listener
+        start.setBounds(0,0,200,100); //dimensions
+        //start.setIcon(); //the image present on the button
+
+        this.add(start); //adds the start button to the frame
+        this.setVisible(true);
+
+
     }
 
-    public void paint(Graphics g) {
+    /********************* ACTION PERFORMED METHOD *******************/
 
-       // g.setColor(new Color(232,254,254));
-        //g.fillRect(0,0,1280,720);
-       // g.drawImage(title,439,80,403,286,null);//for some reason it only showTos up after I click;
-        g.drawImage(startback,0,0,getWidth(),getHeight(),null);
+    public void actionPerformed(ActionEvent e)
+    {
+        if(e.getSource() == start)
+        { //this will close the MainFrame and open a GameFrame when the start button is clicked
+            this.dispose();
+            //new GameFrame();
+        }
+
     }
-    public void drawStart(){
 
+    /*************************** PAINT METHOD **************************/
 
-        //start.setText("Start");
-
-
+    public void paint(Graphics g)
+    {
+        //whatever goes in here, shall go in here
     }
 
 }
